761029ffe98d7fd291d7b45a8771e41f89753850
John Nguyen <nguyenj1863@gmail.com>
2025-08-09 16:23:16 -0400

    created own logger middleware cause the redux logger bundles everything when outputting - scared I might get confused down the line

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,21 @@
 import { compose, createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
-
 import { rootReducer } from './root-reducer';
 
-const middleWares = [logger];
+const loggerMiddleware = (store) => (next) => (action) => {
+    if(!action.type) {
+        return;
+    }
+
+    console.log('type: ', action.type);
+    console.log('payload: ', action.payload);
+    console.log('current state: ', store.getState());
+
+    next(action);
+
+    console.log('new state: ', store.getState());
+}
+
+const middleWares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
