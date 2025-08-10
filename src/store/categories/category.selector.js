0bfff9e2e6e327cca82c135722337bef4eaad7b2
John Nguyen <nguyenj1863@gmail.com>
2025-08-10 00:19:10 -0400

    added reselect library for memoization in category.selector to improve time use for the reduce function

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,8 +1,20 @@
-export function selectCategoriesMap(state) {
-    return state.categories.categories
-        .reduce((acc, category) => {
-        const { title, items } = category;
-        acc[title.toLowerCase()] = items;
-        return acc;
-    }, {});
+import { createSelector } from "reselect";
+
+function selectCartegoryReducer(state) {
+    return state.categories;
 }
+
+export const selectCategories = createSelector(
+    [selectCartegoryReducer],
+    (categoriesSlice) => categoriesSlice.categories
+)
+
+export const selectCategoriesMap = createSelector(
+    [selectCategories],
+    (categories) => 
+        categories.reduce((acc, category) => {
+            const { title, items } = category;
+            acc[title.toLowerCase()] = items;
+            return acc;
+    }, {})
+);
