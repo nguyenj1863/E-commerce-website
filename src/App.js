488d2446b04dbdf2c290c39c36d6d541c5c32647
John Nguyen <nguyenj1863@gmail.com>
2025-08-10 20:34:12 -0400

    transitioned user reducer code into a user slice

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
-import { setCurrentUser } from './store/user/user.action';
+import { setCurrentUser } from './store/user/user.reducer';
 
 function App() {
   const dispatch = useDispatch();
@@ -23,6 +23,7 @@ function App() {
         if (user) {
           createUserDocumentFromAuth(user);
         }
+        console.log(user);
         dispatch(setCurrentUser(user));
       });
   
--- src/store/store.js
+++ src/store/store.js
@@ -3,7 +3,7 @@ import { configureStore } from '@reduxjs/toolkit';
 
 // import { persistStore, persistReducer } from 'redux-persist';
 // import storage from 'redux-persist/lib/storage';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
 
